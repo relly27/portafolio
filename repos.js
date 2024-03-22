9492c331812d3842d98e575fe7fa9c19d51c96a6
relly27 <rellyalbertogarciapacheco_@hotmail.com>
2024-03-22 16:09:35 -0400

    Initial commit

--- repos.js
+++ repos.js
@@ -1,6 +1,7 @@
 let url = "https://api.github.com/users/relly27/repos";
-let repos;
 let jobs;
+
+
 
 fetch(url)
     .then(resp => {
@@ -10,20 +11,48 @@ fetch(url)
       return resp.json();
     })
     .then(data => {
-      console.info(data);
-      repos = data;
       
-      jobs = repos.map(item => {
+      jobs = data.map(item => {
         return {
             nombre: item.name,
             api_repo: item.contents_url.replace(/{\+path}/g, ''),
             html_url: item.html_url,
-            cover: `https://raw.githubusercontent.com/relly27/${item.name}/main/cover.png`
+            // cover: `https://raw.githubusercontent.com/relly27/${item.name}/${item.default_branch}/cover.png`
+            cover: ""
         };
     });
 
+    for (let i = 0; i < jobs.length; i++) {
+
+      fetch(jobs[i].api_repo)
+          .then(respon => {
+              if (!respon.ok) {
+                  throw new Error(`HTTP error! Status: ${resp.status}`);
+              }
+              return respon.json();
+          })
+          .then(dat => {
+
+            dat.forEach(item => {
+               if (item.name === "cover.png") {
+                    jobs[i].cover = item.download_url
+                }
+            });
+
+          })
+      
+      for (let j = 0; j < jobs.length; j++) {
+        if(jobs[i].cover == ""){
+          jobs[i].cover = "noAvailable.jpg";
+        }
+        
+      }
+
+    }
 
     })
     .catch(e => console.error("Algo va mal", e));
+
+    
 
     
