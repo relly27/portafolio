03ab7d7f7be1f324fac0e35f320951fc4697e3c4
relly <rellyalbertogarciapaacheco_@hotmail.com>
2024-03-21 23:31:10 -0400

    added object jobs

--- repos.js
+++ repos.js
@@ -1,5 +1,6 @@
 let url = "https://api.github.com/users/relly27/repos";
 let repos;
+let jobs;
 
 fetch(url)
     .then(resp => {
@@ -11,6 +12,18 @@ fetch(url)
     .then(data => {
       console.info(data);
       repos = data;
+      
+      jobs = repos.map(item => {
+        return {
+            nombre: item.name,
+            api_repo: item.contents_url.replace(/{\+path}/g, ''),
+            html_url: item.html_url,
+            cover: `https://raw.githubusercontent.com/relly27/${item.name}/main/cover.png`
+        };
+    });
+
 
     })
     .catch(e => console.error("Algo va mal", e));
+
+    
